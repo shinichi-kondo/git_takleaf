b238768a1cb4b57df7b1f18a28c272cec1fcd1c0
EC2 Default User <ec2-user@ip-172-31-93-111.ec2.internal>
2019-10-22 05:56:16 +0000

    アプリ完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,12 @@ class TasksController < ApplicationController
   
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(distinct: true).page(params[:page])
+    
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%d%S')}.csv"}
+    end
   end
 
   def show
@@ -25,6 +30,7 @@ class TasksController < ApplicationController
     end
     
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
@@ -44,12 +50,17 @@ class TasksController < ApplicationController
   def confirm_new
     @task = current_user.tasks.new(task_params)
     render :new unless @task.valid?
+  end
+  
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to task_url, notice: "タスクを追加しました"
   end  
   
   private
   
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
   
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+    has_one_attached :image
+    
     def self.ransack_attributes(auth_object = nil)
       %w[name created_at]
     end
@@ -15,6 +17,26 @@ class Task < ApplicationRecord
     
     scope :recent, -> { order(created_at: :desc)}
     
+    def self.csv_attibutes
+      ["name", "description", "created_at", "updated_at"]
+    end
+    
+    def self.generate_csv
+      CSV.generate(headers: true) do |csv|
+        csv << csv_attibutes
+        all.each do |task|
+          csv << csv_attibutes.map{|attr| task.send(attr)}
+        end
+      end
+    end
+    
+    def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+        task = new
+        task.attributes = rows.to_hash.slice(*csv_attibutes)
+        task.save!
+      end
+    end  
   
 
     private
